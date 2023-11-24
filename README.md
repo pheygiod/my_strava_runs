2326b9c286701dfbd568cde40ecb2c01f4086aa9
giorgia dimiccoli <giorgiadt14@gmail.com>
2023-11-24 16:32:34 +0000

    uploading refactored code

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # my_strava_runs
-Scraping data from my personal Strava account via the API they provide and creating data visualisations and map routes! 
+Scraping data from my personal Strava account via the API they provide and creating data visualisations and map routes!🏃
+
+# Table of Contents
+- General Info
+- Setup
+- Usage
+- Project Status
+- Room for Improvement
+- Acknowledgements
+- Contact
+
+# General Information 
+I’m uploading my code of the physical activities I analysed using Strava's APIs!💻🏈 The goal was to explore the activities I tracked and uploaded onto my Strava account and see if I could make some nice plots. With this initiative I also want to support other coders who are sports fanatics like me and are willing to plot your own maps!🗺️🔎👣
+
+The data I extracted is made up of 137 rows and 58 columns. I helped myself with [this](https://towardsdatascience.com/using-the-strava-api-and-pandas-to-explore-your-activity-data-d94901d9bfde) article, which explains how to get the data through Strava's APIs. I got the data, cleansed it🛁, and started exploring it.
+
+# Setup
+First off, make sure you have conda🐍👀:
+
+`conda create -n <replace-with-name-you-want> python=3.11`
+
+`conda activate <replace-with-name-you-want>`
+
+`pip install -r requirements.txt`
+
+
+# Usage 
+Check out the py files in my repo to see how I've processed the data, exported the gpx files, and created the maps!
+
+- data_processing.py🎰 
+- gpx_files_processing.py📂
+- geospatial_data_processing.py🌏 
+
+For a full walkthrough of the project, check out the `strava_project.ipynb` file!📌
+
+# Project Results 
+I discovered that my most frequent activity in the past couple of years was running, and the most common run distance was 5km🏃. I also found out that my average pace decreased during my short runs. Probably because I was training for a marathon. Last but not least, Oxfordshire is the county where I exercised the most since I live here🏡🇬🇧. 
+
+# Future Data Exploration Ideas
+For future use, we could design some time series visualisations to track our fitness progress over time⏰(e.g., heart rate, calories burned, etc.). We could also create a radar chart to compare our runs with the ones of our partners/friends'📊. We could even plot a chart to compare the ranking of teams during sports tournaments🥇(e.g., metrics could include defense, offense, rating). For me, the sport would be padel🎾. For you, it can be any sport/activity that you like!
+
+# Acknowledgements
+A massive thank you to `Matt Ambrogi` for the awesome tutorial on diving into Strava data with Python APIs. Your insights have been a game-changer for me!
+
+Big kudos to `Meg` from Strava for the fantastic tutorial on bulk-export data through APIs – your efforts have made the process so much clearer and user-friendly!
+
+Last but not least, a special shoutout to `Ward Haddadin` for always motivating me in my coding journey! Ward, your help in checking my code and pushing me do big things has been invaluable. 
+
+# Contact
+For any question, drop me a line at giorgiadt14@gmail.com and I'll be happy to help you out! Feel free to message me on [LinkedIn](https://www.linkedin.com/in/giorgia-dim/) too! Happy coding!
---
+++ data_processing.py
@@ -0,0 +1,150 @@
+import calendar
+import matplotlib.pyplot as plt
+import pandas as pd
+import requests
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+AUTH_URL = "https://www.strava.com/oauth/token"
+ACTIVITIES_URL = "https://www.strava.com/api/v3/athlete/activities"
+
+def getting_access_and_refresh_tokens(client_id, client_secret, refresh_token):
+    """
+    Function to do bla. has 3 args, must be strings
+    """
+    payload = {
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'refresh_token': refresh_token,
+        'grant_type': "refresh_token",
+        'f': 'json'
+    }
+    
+    print("Requesting Token...\n")
+    res = requests.post(AUTH_URL, data=payload, verify=False)
+    access_token = res.json()['access_token']
+    
+    return access_token
+   
+
+def get_dataset(access_token):
+    header = {'Authorization': 'Bearer ' + access_token}
+    param = {'per_page': 200, 'page': 1}
+    my_dataset = requests.get(ACTIVITIES_URL, headers=header, params=param).json()
+    # Creating a dataframe
+    df = pd.DataFrame(my_dataset)
+    return df
+
+
+# Converting the moving_time column into hours, minutes, and seconds
+def convert_to_hhmmss(moving_time): # Creating a function that takes in as input a moving time 
+    # converting seconds into integers 
+    total_seconds = int(moving_time)
+    
+    # dividing seconds and assigning the final number to hours 
+    hours = total_seconds // 3600
+    
+    # converting total number of seconds into minutes 
+    minutes = (total_seconds % 3600) // 60 # computing the remainder when total_seconds is divided by 3600
+    
+    # calculating the number of seconds 
+    seconds = total_seconds % 60 # dividing total_seconds by 60 (which is the amount of seconds in a minute)   
+    return f"{hours:02d}:{minutes:02d}:{seconds:02d}"
+
+# Splitting the start date into year, month, day, and time
+def start_date_to_y_m_d_t(start_date):
+    split_start_date = start_date.split("T")
+    y_m_d = split_start_date[0]
+    split_y_m_d = y_m_d.split("-")
+    year = split_y_m_d[0]
+    month = split_y_m_d[1]
+    day = split_y_m_d[2]
+    time = split_start_date[1].rstrip("Z")
+    
+    return pd.Series({
+        "year" : int(year),
+        "month" : int(month),
+        "day" : int(day),
+        "time" : time
+    })
+
+def converting_num_months_to_strings(df):
+    # Swapping numeric 'month' with its corresponding name
+    df['month'] = df['month'].apply(lambda x: calendar.month_name[x]) # converting numeric month to month names
+    # Defining the custom month order
+    custom_month_order = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    # Converting the 'month' column to categorical with custom order
+    df['month'] = pd.Categorical(df['month'], categories=custom_month_order, ordered=True)
+    return df
+
+def sorting_values_by_year(df):
+    df = df.sort_values("year")
+    return df
+
+# Converting metres to km
+def converting_metres_into_kms(df):
+    df['distance_km'] = df['distance'] / 1000 # Converting metres to km
+    df['distance_km'] = df['distance_km'].round() # Rounding the distances  
+    df.drop(columns=['distance'], inplace=True) # Dropping the original 'distance' column 
+    return df 
+
+def creating_a_total_column(frequency_table):
+    frequency_table['total'] = frequency_table.sum(axis=1)
+    frequency_table.sort_values('total', inplace=True, ascending=False) # sorting the values in the total column
+    frequency_table.drop(columns=["total"], inplace=True) # the total column is not needed anymore 
+    return frequency_table 
+
+# Plotting a frequency table into a grouped bar chart 
+def plotting_table_into_grouped_bar_chart(frequency_table):
+    frequency_table.T.plot(kind='bar', stacked=True, figsize=(10,6))
+    plt.xlabel('Month')
+    plt.ylabel('Frequency')
+    plt.ylim(0, 20) # setting the y-axis limit
+    plt.title('My Sport Activity by Month in 2023 only') # setting a title 
+    plt.legend(title='Sport', bbox_to_anchor=(1.05, 1), loc='upper left') # adjusting the legend's position
+    plt.xticks(rotation=45) # choosing the rotation of the ticks labels 
+    plt.show # showing the plot 
+    return frequency_table
+
+# Removing activities practiced only once or twice
+def remove_rows_by_sport_type(filtered_df, sport_types_to_remove):
+    filtered_df = filtered_df[~filtered_df['sport_type'].isin(sport_types_to_remove)]
+    return filtered_df
+
+# Creating bins using the cut function
+def creating_bins(run_df, column_name, bin_intervals, labels=None):
+    run_df['distance_bins'] = pd.cut(run_df[column_name], 
+                                                   bins=bin_intervals, labels=labels
+                                                  )
+    return run_df
+
+# Calculating basic statistics
+def calculating_basic_statistics(run_df, column_name):
+    column_data = run_df[column_name]
+    statistics_dict = {
+        'median': column_data.median(),
+        'mean': column_data.mean(),
+        'std': column_data.std()
+    }
+    return statistics_dict
+
+# Plotting a histogram
+def plotting_a_histogram(run_df, column_name):
+    plt.figure(figsize=(10,6))
+    plt.hist(run_df['distance_km'], bins=bin_intervals, edgecolor='Blue', alpha=0.7)
+    
+    # Adding vertical lines for mean and median
+    plt.axvline(mean_distance, color='red', linestyle='dashed', linewidth=1, label='Mean')
+    plt.axvline(median_distance, color='black', linestyle='dashed', linewidth=1, label='Median')
+    
+    # Setting labels and title
+    plt.xlabel('Distance (km)')
+    plt.ylabel('Frequency')
+    plt.title('Distribution of my Run\'s Distances')
+    plt.legend()
+    
+    # Displaying the plot
+    plt.show()
+    return run_df
